729973f80b0f735d21d5054f921b2eb800449c34
kevin <kevinkalenga10@gmail.com>
2026-01-21 00:47:17 +0100

    Pricing - Part3

--- resources/views/admin/package/create.blade.php
+++ resources/views/admin/package/create.blade.php
@@ -22,15 +22,13 @@
                 
                 
                 <div class="section-body">
+                  <form id="dynamicForm" action="{{route('admin_package_store')}}" method="post" >
+                    @csrf
                     <div class="row">
-                        <div class="col-12">
+                        <div class="col-md-4">
                             <div class="card">
                                 <div class="card-body">
-                                    <form action="{{route('admin_package_store')}}" method="post" >
-                                        @csrf
-                                        
-                                           
-                                         
+                                    
                                             <div class="mb-4">
                                                 <label class="form-label">Name *</label>
                                                 <input type="text" class="form-control" name="name" value="{{old('name')}}">
@@ -52,20 +50,59 @@
 
                                             
                                             
-                                           <div class="mb-4">
-                                                    <label class="form-label"></label>
-                                                    <button type="submit" class="btn btn-primary">Submit</button>
-                                            </div>
-                                          
-                                       
-                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                           <div class="card">
+                                <div class="card-body">
+                                    <h5>Facilities</h5>
+                                    <button type="button" class="btn btn-warning btn-sm mb-2" id="addRowButton">Add Row</button>
+                                    <div class="item mb-2">
+                                    <div class="row">
+                                        <div class="col-md-5">
+                                            <input type="text" class="form-control" name="facility[]" placeholder="Facility Name Here">
+                                        </div>
+                                        <div class="col-md-3">
+                                            <select name="status[]" class="form-select">
+                                                <option value="Yes">Yes</option>
+                                                <option value="No">No</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-md-2">
+                                            <input type="text" class="form-control" name="order[]" placeholder="Order">
+                                        </div>
+                                        <div class="col-md-2">
+                                            <button type="button" class="btn btn-danger del"><i class="fas fa-trash"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                                 <div id="newItemHere"></div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <div class="col-md-12">
+                      <div class="mb-4">
+                        <label class="form-label"></label>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                      </div>
+                    </div>
+                 </form>
                 </div>
             </section>
         </div>
 
-    </div>
+</div>
+<script>
+    $(document).ready(function() {
+        $('#addRowButton').click(function() {
+            var newRow = $('<div class="item mb-2"><div class="row"><div class="col-md-5"><input type="text" class="form-control" name="facility[]" placeholder="Facility Name Here"></div><div class="col-md-3"><select name="status[]" class="form-select"><option value="Yes">Yes</option><option value="No">No</option></select></div><div class="col-md-2"><input type="text" class="form-control" name="order[]" placeholder="Order"></div><div class="col-md-2"><button type="button" class="btn btn-danger del"><i class="fas fa-trash"></i></button></div></div></div>');
+            $('#newItemHere').append(newRow);
+        });
+        $(document).on('click', '.del', function() {
+            $(this).closest('.item').remove();
+        });
+    });
+</script>
 @endsection
